9076265a4dc5f5378ebca9b7f908f5a2b725d2c3
Yara-Abdelrahem <yaraomran961@gmail.com>
2024-11-17 17:07:16 +0200

    update files

---
+++ Add_Function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float AddFunction (float x,float y){
+	
+	return x+y;
+}
+
+
+double AddFunction (float x,float y)
+{
+	
+	return x+y;
+}
+
+
+
--- main.c
+++ main.c
@@ -1,4 +1,48 @@
-int main(void)
-{
+#include "stdio.h"
+#include "conio.h"
+#include "division.c"
+#include "sub.c"
+#include "multiplication.c"
 
+void scan_input(float*, float*);
+
+int main(void) {
+    do {
+        char c;
+        float a, b;
+        printf("\033c"); // Clear screen
+
+        printf("Please enter your preferred operation: + - * / \n");
+        c = getch();
+
+        switch (c) {
+            case '/':
+                scan_input(&a, &b);
+                printf("a/b = %.2f\n", Division(a, b));
+                break;
+
+            case '-':
+                scan_input(&a, &b);
+                printf("a-b = %.2f\n", subtract(a, b));
+                break;
+
+            case '*':
+                scan_input(&a, &b);
+                printf("a*b = %.2f\n", multiplication(a, b));
+                break;
+
+            default:
+                printf("Invalid operation\n");
+        }
+
+        printf("Press any key to continue. To exit, press 'n'.\n");
+    } while (getch() != 'n');
+
+    return 0;
+}
+
+void scan_input(float* a, float* b) {
+    printf("Enter a:\n");
+    fflush(stdout);
+    scanf("%f", b); // Dereference the pointer
 }
